e61ae3e8f5d887321c8bc41dc4e63add9fc2f9e9
elliejaneeeeee <145994573+elliejaneeeeee@users.noreply.github.com>
2024-06-09 03:40:02 +0100

    add /api endpoint

--- routes/api-router.mjs
+++ routes/api-router.mjs
@@ -1,7 +1,12 @@
 import express from 'express'
 import usersRouter from './users-router.mjs'
+import endpoints from "../endpoints.json" assert { type: "json" };
 
 const apiRouter = express.Router()
+
+apiRouter.get('/', (req, res) => {
+    res.status(200).send({endpoints})
+})
 
 apiRouter.use('/users', usersRouter)
 
