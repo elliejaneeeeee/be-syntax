c72417d6c867cd1916d8889e10da5add0072e5e1
elliejaneeeeee <145994573+elliejaneeeeee@users.noreply.github.com>
2024-06-27 22:55:01 +0100

    implement path for post new user

--- controllers/users.controller.mjs
+++ controllers/users.controller.mjs
@@ -1,22 +1,33 @@
-import { findUsers, findUser } from "../models/users.model.mjs"
-import { isValidId } from "../utils/isValidId"
-
+import { findUsers, findUser, insertUser } from "../models/users.model.mjs";
+import { isValidId } from "../utils/isValidId.mjs";
+import { isAllFieldsValid } from "../utils/isAllFieldsValid.mjs";
 
 export const getAllUsers = async (req, res, next) => {
-    const users = await findUsers()
-    res.status(200).send({users})
-}
+  const users = await findUsers();
+  res.status(200).send({ users });
+};
 
 export const getAUser = async (req, res, next) => {
-    const {id} = req.params
-    if (!isValidId(id)) {
-        return res.status(400).send({msg: "400 Error: Bad Request"})
-    }
+  const { id } = req.params;
+  if (!isValidId(id)) {
+    return res.status(400).send({ msg: "400 Error: Bad Request" });
+  }
 
-    const user = await findUser(id)
+  const user = await findUser(id);
 
-    if (user.length === 0) {
-        return res.status(404).send({msg: "404 Error: Resource Not Found"})
-    }
-    res.status(200).send({user})
-}
+  if (user.length === 0) {
+    return res.status(404).send({ msg: "404 Error: Resource Not Found" });
+  }
+  res.status(200).send({ user });
+};
+
+export const postUser = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (!isAllFieldsValid(email, username, password)){
+    return res.status(400).send({ msg: "400 Error: Bad Request" })
+  }
+
+  await insertUser(username, email, password);
+  res.status(201).send({ msg: "201: Successfully Created Resource" });
+};
--- models/users.model.mjs
+++ models/users.model.mjs
@@ -1,13 +1,34 @@
-import { connectToDb } from '../server/db/connection.mjs'
+import { connectToDb } from "../server/db/connection.mjs";
+import { createNewUserId } from "../utils/createNewUserId.mjs";
 
-const db = await connectToDb()
+const db = await connectToDb();
 
 export const findUsers = async () => {
-    const users = await db.collection("users").find({}).toArray()
-    return users
-}
+  const users = await db.collection("users").find({}).toArray();
+  return users;
+};
 
 export const findUser = async (id) => {
-    const user = await db.collection("users").find({ "user_id": id}).toArray()
-    return user
-}
+  const user = await db.collection("users").find({ user_id: id }).toArray();
+  return user;
+};
+
+export const insertUser = async (username, email, password) => {
+  const newId = await createNewUserId();
+
+  const newUserObj = {
+    user_id: newId,
+    username,
+    email,
+    password,
+    join_date: new Date(),
+    profile_picture: `https://ui-avatars.com/api/?name=${username}`,
+    bio: "",
+    languages: [],
+    achievements: [],
+    settings: { notifications: { email: false, phone: false } },
+  };
+
+  const response = await db.collection("users").insertOne(newUserObj);
+  return response
+};
