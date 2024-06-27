fff22183f58f03dfcc6214ba89d60f4145dfe6a2
elliejaneeeeee <145994573+elliejaneeeeee@users.noreply.github.com>
2024-06-27 22:46:58 +0100

    Add express.json for incoming body parsing

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from 'express'
 import apiRouter from './routes/api-router.mjs'
 const app = express()
 
+app.use(express.json())
 app.use('/api', apiRouter)
 
 app.all('*', (req, res) => {
